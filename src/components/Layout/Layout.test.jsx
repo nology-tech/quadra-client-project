a5d77a34c1097800a447bed13fd5da907c98027d
Dan Forder <danfrdr@gmail.com>
2022-10-19 09:32:35 +0100

    add tests

---
+++ src/components/Layout/Layout.test.jsx
@@ -0,0 +1,24 @@
+import { customRender } from "../../utils/testUtils";
+import Layout from "./Layout";
+
+it("Should render layout with children", () => {
+  const { container } = customRender(
+    <Layout>
+      <p>some content</p>
+    </Layout>
+  );
+
+  expect(container).toMatchSnapshot();
+});
+
+
+it("Should render without navbar when showNavbar is false", () => {
+  const { container } = customRender(
+    <Layout showNavbar={false}>
+      <p>some content</p>
+    </Layout>
+  );
+
+  expect(container).toMatchSnapshot();
+});
+
