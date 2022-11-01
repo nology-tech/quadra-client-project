8047be9e39841703e311dbaf0b14f1e2e59e90ac
Isaac-D-Dawson <isaacdanieldawson1837@gmail.com>
2022-11-01 16:14:43 +0000

    Adjusted React element styling.

--- src/pages/Transfer/Transfer.jsx
+++ src/pages/Transfer/Transfer.jsx
@@ -1,30 +1,11 @@
 import "./Transfer.scss";
 
 const Transfer = ({ currencyFlag, currencyName, amount, rate, isActive }) => {
-    <div 
-        className={isActive?"transfer transfer__active" : "transfer"}
-    >
-        <img
-            className="transfer__flag"
-            src={currencyFlag}
-        />
-        <p
-            className="transfer__currency"
-        >
-            {currencyName}
-        </p>
-        <p
-            className="transfer__amount"
-        >
-            {amount}
-        </p>
-        {
-            rate&&<p
-                className="transfer__rate"
-            >
-                {rate}
-            </p>
-        }
+    <div className={isActive?"transfer transfer__active" : "transfer"}>
+        <img className="transfer__flag" src={currencyFlag}/>
+        <p className="transfer__currency">{currencyName}</p>
+        <p className="transfer__amount"> {amount}</p>
+        {rate&&<p className="transfer__rate">{rate}</p>}
         {/* Put button component here when we have a button element. */}
         {/* Button text/effect needs to be different depending on isActive */}
         {/* button class should be "transfer__object__button" for SCSS compatability. */}
