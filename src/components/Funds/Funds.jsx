9abd1896d3acc6fb47bd61e525ba3e3155717050
Lewis Lovett <llovett7575@gmail.com>
2022-11-02 11:00:35 +0000

    Updated button styles

--- src/components/Funds/Funds.jsx
+++ src/components/Funds/Funds.jsx
@@ -1,12 +1,12 @@
 import "./Funds.scss";
 import Button from '../Button/Button';
 
-const Funds = ({amount}) => {
+const Funds = ({amount, isManage}) => {
     return(
         <div className="funds">
-            <h4 className="fund__title">Your Funds</h4>
-            <p className="fund__amount">{amount}</p>
-            <Button buttonText={"Manage"}/>
+            <h4 className="funds__title">Your Funds:</h4>
+            <p className="funds__amount">£{amount}</p>
+            {isManage && <Button buttonText={"Manage"}/>}
         </div>
     )
 }
