245b7c570fbbae1e4c48797fa9bf03bd58cc6771
haoxle <hao-le@live.co.uk>
2022-11-02 16:11:57 +0000

    firebase data logic added

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDCGk-Jhijs3MwEcQd55WFsFwzlWUVRfYE",
+  authDomain: "quadra-client-project.firebaseapp.com",
+  projectId: "quadra-client-project",
+  storageBucket: "quadra-client-project.appspot.com",
+  messagingSenderId: "1060709714462",
+  appId: "1:1060709714462:web:68ea128921bb72245d3c0f",
+  measurementId: "G-HQBN4F7THC",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,52 @@
 import Layout from "../../components/Layout/Layout";
-
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from "../../firebase";
+import { useState } from "react";
 const Home = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const auth = getAuth(app);
+
+  const toFirebase = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        alert("user has been created");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        // const errorMessage = error.message;
+        // // ..
+        alert(errorCode);
+      });
+  };
+
   return (
     <Layout>
-      <h1>Page Heading</h1>
-      <h2>Section Heading</h2>
-      <h3>Panel Heading</h3>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error
-        dolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatis
-        maxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
-        necessitatibus.
-      </p>
-      <p>Ollie change to test auto deployment</p>
-      <p>Showing PR request example</p>
+      <label>
+        Email Address
+        <input
+          type={"email"}
+          placeholder="please enter your email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+
+      <label>
+        password
+        <input
+          type={"password"}
+          placeholder="please enter your password"
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+      </label>
+
+      <button onClick={toFirebase}>Sign up</button>
+
+      <h1>You have signed up!</h1>
     </Layout>
   );
 };
