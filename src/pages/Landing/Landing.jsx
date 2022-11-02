cfbd9aa026a1df6fd0e8804bd38dee16442ba0a9
Lewis Lovett <llovett7575@gmail.com>
2022-11-02 15:25:38 +0000

    Completed top section

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,13 +1,19 @@
 import './Landing.scss';
+import coverterScreenshot from "../../assets/images/converter-screenshot.png"
 
 
 const Landing = () => {
    return(
+    <>
     <section className='top-section'>
-        <h1>Lorem ipsum dolor, sit amet consectetur.</h1>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <img src="../../assets/images/converter-screenshot.png" />
+        <h1 className='top-section__title'>Lorem ipsum dolor sit amet, consectetur <span className='top-section__highlight'>adipiscing elit.</span></h1>
+        <p className='top-section__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, quas ad explicabo.</p>
+        <img className='top-section__screenshot' src={coverterScreenshot} alt='Converter Screenshot' />
     </section>
+    <section className='bank-section'>
+        <h2></h2>
+    </section>
+    </>
    )
 } 
 export default Landing;
