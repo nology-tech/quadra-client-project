ca5500e809bb9e2156826c3532910c47f056fac6
Nicholas <blueretrograde@gmail.com>
2022-11-02 16:59:45 +0000

    added the input box components

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,53 +1,67 @@
-import "./SignUp.scss"
+import "./SignUp.scss";
 import Button from "../../components/Button/Button";
-import logo from "../../assets/images/logo.png"
+import logo from "../../assets/images/logo.png";
+import InputBox from "../../components/InputBox/InputBox";
 
-const SignUp = ({login}) => {
+const SignUp = ({ login }) => {
+  return (
+    <div className="background">
+      <div className="signUp">
+        <div className="signUp__company">
+          <img
+            className="signUp__company signUp__company--logo"
+            src={logo}
+          ></img>
+          <p className="signUp__company signUp__company--name">Synergy</p>
+        </div>
 
-    
-    return(
-
-        <div className="background" >
-        <div className="signUp">
-            <div className="signUp__company">
-            <img className="signUp__company signUp__company--logo" src={logo} ></img>
-            <p className="signUp__company signUp__company--name">Synergy</p>
-            </div>
-
-            <h1 className="signUp__header" >Create an account</h1>
-            <p className="signUp__headerText" > Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+        <h1 className="signUp__header">Create an account</h1>
+        <p className="signUp__headerText">
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+        </p>
 
         <div className="signUp__input">
-            {/* Input box */}
-            <p>Name</p>
-            <input type="text" />
-            
-            {/* Input box */}
-            <p>Password</p>
-            <input type="text" />
+          <InputBox
+            className="signUp__input signUp__input--email"
+            title="Email"
+            inputType="text"
+            errorMessage="Error - your email must contain an '@' symbol"
+            successMessage="Successful email entered"
+          />
 
-            {/* Input box */}
-            <p>Confirm Password</p>
-            <input type="text" />
+          <InputBox
+            className="signUp__input signUp__input--password"
+            title="Password"
+            inputType="password"
+            errorMessage="Error- Passwords must contain at least 8 characters & one uppercase letter"
+            successMessage="Password meets requirements"
+          />
 
-        </div>    
-        
-
-            <div className="signUp__text">
-            <p className="signUp__text signUp__text--account">Already have an account?</p>
-            <a href={login} className="signUp__text signUp__text--login">Login</a>
-            </div>
-
-            <div className="signUp__continue" >
-            <Button buttonText="Continue >"/>
-            </div>
-
-
-
+          <InputBox
+            className="signUp__input signUp__input--confirmPassword"
+            title="Confirm Password"
+            inputType="password"
+            errorMessage="Error - Passwords do not match!"
+            successMessage="Passwords match"
+          />
         </div>
+
+        <div className="signUp__text">
+          <p className="signUp__text signUp__text--account">
+            Already have an account?
+          </p>
+          <a href={login} className="signUp__text signUp__text--login">
+            Login
+          </a>
         </div>
- 
-    )
-}
+
+        <div className="signUp__continue">
+          <Button buttonText="Continue >" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SignUp;
