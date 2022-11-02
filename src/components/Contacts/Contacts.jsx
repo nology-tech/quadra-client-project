714efb2f446bd9ee602f7ab356a2b0dcde74b6ba
Anusha Antony <anushaca1995@gmail.com>
2022-11-02 11:58:15 +0000

    Made changes to scss for Contacts component

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -4,13 +4,13 @@ const Contacts = ({bankImage, userName, sortCode, accNo, bankName, iban, deleteI
 
     return(
         <div className="contacts">
-            <img className="contacts__bankImage" src={bankImage}></img>
+            <img className="contacts__Image contacts__Image--bankImage" src={bankImage}></img>
             <p className="contacts__userName">{userName}</p>
             <p className="contacts__sortCode">{sortCode}</p>
             <p className="contacts__accNo">{accNo}</p>
             <p className="contacts__bankName">{bankName}</p>
             <p className="contacts__iban">{iban}</p>
-            <img className="contacts__deleteIcon" src={deleteIcon}></img>
+            <img className="contacts__Image contacts__Image--deleteIcon" src={deleteIcon}></img>
         </div>
     )
 }
