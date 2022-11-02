538a3c793f41205e779d722d2d1075a09f0e0fe3
Chris Wakley <chris.wakley666@gmail.com>
2022-11-02 10:03:41 +0000

    minor modification before pull request.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-
 import "./styles/main.scss";
 
 const App = () => {
   return (
-    <Nav />
-    // <Routes>
-    //   <Route path="*" element={<Home />} />
-    // </Routes>
+    <Routes>
+      <Route path="*" element={<Home />} />
+    </Routes>
   );
 };
 
