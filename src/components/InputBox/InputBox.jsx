ef11b10793e5911286c1640548d045822f2c492d
Lewis Lovett <llovett7575@gmail.com>
2022-11-02 09:27:42 +0000

    Added new variables and imported into InputBox

--- src/components/InputBox/InputBox.jsx
+++ src/components/InputBox/InputBox.jsx
@@ -2,11 +2,11 @@ import './InputBox.scss';
 
 const InputBox = ({title , inputType, errorMessage, successMessage}) => {
     return(
-        <div className='input-container'>
-            <p className='input-container__title'>{title}</p>
-            <input className='input-container__input' type={inputType}></input>
-            <p className='input-container__message input-container__message--error'>{errorMessage}</p>
-            <p className='input-container__message input-container__message--success'>{successMessage}</p>
+        <div className='input'>
+            <p className='input__title'>{title}</p>
+            <input className='input__input' type={inputType}></input>
+            <p className='input__message input__message--error'>{errorMessage}</p>
+            <p className='input__message input__message--success'>{successMessage}</p>
         </div>
     )
 }
