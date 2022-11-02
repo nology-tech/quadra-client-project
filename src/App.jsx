05cfac38c553d1b668a245e12e5375e6c0c54702
Chris Wakley <chris.wakley666@gmail.com>
2022-11-02 11:07:26 +0000

    merge conflicts resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-// import { Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home/Home";
-=======
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-
->>>>>>> bcfa3621e49517cfb86e3b4db7bd93d458dc32ff
 import "./styles/main.scss";
 
 const App = () => {
