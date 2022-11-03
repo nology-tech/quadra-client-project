0bff96761827bfaed729f39fed745ac58dbd1a84
Martinelli-89 <99336076+Martinelli-89@users.noreply.github.com>
2022-11-03 10:03:35 +0000

    removed inline style and added scss style

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,13 @@
 import LogoImg from "../../assets/images/logo.png";
 import "./Logo.scss";
 
-const Logo = ({isTextDark, width, height}) => {
+const Logo = ({isTextDark}) => {
 
     return (
 
-        <div className="logo" style={{width: `${width}rem`, height: `${height}rem`}}>
+        <div className="logo">
             <img src={LogoImg} alt="Synergy Logo" className="logo__logo"></img>
-            <p className={isTextDark ? "logo__darkText" : "logo__lightText"} style={{fontSize:`${height/2.5}rem`}}>Synergy</p>
+            <p className={isTextDark ? "logo__darkText" : "logo__lightText"}>Synergy</p>
         </div>
 
     );
