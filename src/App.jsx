e2801acbb0f66c928bb0e7f5d5c0347065f24a30
lanniiee <lav250397@hotmail.com>
2022-11-03 15:09:07 +0000

    routing for sign up and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "./styles/main.scss";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp"
 
 const App = () => {
   return (
     <Routes>
       <Route path="*" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp/>} />
     </Routes>
   );
 };
