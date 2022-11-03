9f396c94694bf8d470b5f4853f4200f4394ea35a
Anna Kurochkina <basia220586@gmail.com>
2022-11-03 17:43:27 +0000

    CurrencyConverter component is partially done (work in progress)
    * Added a CurrencyIcons.js file into the Utils folder, for mapping currency strings into country flag components.
    * Implemented a Dropdown component from Suite library https://rsuitejs.com/components/dropdown/   https://www.geeksforgeeks.org/react-suite-dropdown-dropdown-with-icon/
    * Styling needed
    * App.jsx has been modified for testing (remember to undo when ready to merge into main)
    * In file .eslintrc.json added a rule for testing purposes(not to throw error about unused variables)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
+// import { Route, Routes } from "react-router-dom";
+// import Home from "./pages/Home/Home";
+
+import CurrencyConverter from "./components/CurrencyConverter/CurrencyConverter";
+import {useState} from "react";
 
 import "./styles/main.scss";
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="*" element={<Home />} />
-    </Routes>
+  const dummyCurrencyList = ["GBP", "USD", "EUR", "JPY", "ZAR", "AUD"];
+
+  const [amount, setNewAmount] = useState(10);
+  const [fromCurrency, setFromCurrency] = useState(dummyCurrencyList[0]);
+  const [toCurrency, setToCurrency] = useState(dummyCurrencyList[2]);
+
+  return (    
+    // <Routes>
+    //   <Route path="*" element={<Home />} />
+    // </Routes>
+    <CurrencyConverter
+      currencyList={dummyCurrencyList}
+      amount={amount}
+      fromCurrency={fromCurrency}
+      toCurrency={toCurrency}
+      setNewAmount={setNewAmount}
+      setFromCurrency={setFromCurrency}
+      setToCurrency={setToCurrency}
+      />
   );
 };
 
--- src/components/currencyConverter/CurrencyConverter.jsx
+++ src/components/currencyConverter/CurrencyConverter.jsx
@@ -1,7 +1,55 @@
-import "CurrencyConverter.scss";
+import React from "react";
+import { Dropdown } from 'rsuite';
+import iconMap from "../../utils/currencyIcons";
+import Button from "../Button/Button";
 
-const CurrencyConverter = () => {
+import "./CurrencyConverter.scss";
 
+
+const CurrencyConverter = ({currencyList, amount, fromCurrency, toCurrency, setNewAmount, setFromCurrency, setToCurrency}) => {
+    const fromOptions = currencyList.map(currency => <Dropdown.Item key={currency} icon={iconMap[currency]}>{currency}</Dropdown.Item>);
+    const toOptions = currencyList.filter(currency => currency !== fromCurrency).map(currency => <Dropdown.Item key={currency} icon={iconMap[currency]}>{currency}</Dropdown.Item>);
+
+    const handleAmountChange = (event) => setNewAmount(event.target.value);
+    const handleFromChange = (eventKey, event) => {
+        const newFromCurrency = event.target.textContent;
+        if (toCurrency == newFromCurrency) {
+            setToCurrency(currencyList.find(currency => currency != newFromCurrency));
+        }
+        setFromCurrency(newFromCurrency);
+    };
+    const handleToChange = (eventKey, event) => setToCurrency(event.target.textContent);
+
+    return (
+        <div className="converterBox">
+            <div className="convertBox__amount">
+                <label id="test">Amount</label>
+                <input
+                    type="text"
+                    placeholder="£10"
+                    onChange={handleAmountChange}
+                    value={amount}
+                />
+            </div>
+            
+            <div>                
+                <h3>From</h3>                
+                <Dropdown  className="convertBox__currency" title={fromCurrency} icon={iconMap[fromCurrency]} onSelect={handleFromChange} value={fromCurrency}>
+                    {fromOptions}
+                </Dropdown>
+                
+                <Button/>
+
+                <h3>To</h3>
+                <Dropdown className="convertBox__currency" title={toCurrency} icon={iconMap[toCurrency]} onSelect={handleToChange}>
+                    {toOptions}
+                </Dropdown>
+
+                <Button/>
+            </div>
+        </div>
+    );
 }
+
 
 export default CurrencyConverter;
