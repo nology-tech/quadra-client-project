50c758c0115954f5ce0f348464d8444e54b6249b
Chris Wakley <111200375+ChrisWakley@users.noreply.github.com>
2022-11-03 12:05:18 +0000

    Nav/25/creating dashboard (#15)
    Co-authored-by: Zain-Ashfaq <zashfaq00@gmail.com>

--- src/pages/Nav/Nav.jsx
+++ src/pages/Nav/Nav.jsx
@@ -1,34 +1,42 @@
 import "./Nav.scss";
-import Logo from "../../assets/images/logo.png";
+import Logo from "../../components/Logo/Logo";
 import Contacts from "../../assets/images/non-focus-contacts.png";
 import Wallet from "../../assets/images/non-focus-wallet.png";
 import LiveRate from "../../assets/images/non-focus-graph.png";
 import Convert from "../../assets/images/non-focus-convert.png";
+import Transfer from "../../assets/images/transfer-focus.png";
+
 const Nav = () => {
   return (
     <div className="nav">
-      <div className="nav__header">
-        <img src={Logo} alt="logo"></img>
-        <h1>Synergy</h1>
+      <div className="nav__logo">
+        <Logo isTextDark={true}/>
       </div>
-
       <div className="nav__menu">
-        <h1>Menu</h1>
-        <div className="menu__button-row">
+        <h3>Menu</h3>
+        <div className="nav__button">
           <img src={Contacts} alt="contacts"></img>
           <p>Contacts</p>
         </div>
-        <div className="menu__button-row">
+        <div className="nav__button">
           <img src={Wallet} alt="wallet"></img>
           <p>Wallet</p>
         </div>
-        <div className="menu__button-row">
+        <div className="nav__button">
           <img src={LiveRate} alt="graph"></img>
           <p>Live rates</p>
         </div>
-        <div className="menu__button-row">
+        <div className="nav__button">
           <img src={Convert} alt="convert"></img>
           <p>Convert</p>
+        </div>
+        <div className="nav__button">
+          <img src={Transfer} alt="transfer"></img>
+          <p>Transfer</p>
+        </div>
+        <div className="nav__button">
+          <img src={Contacts} alt="contacts"></img>
+          <p>Contacts</p>
         </div>
       </div>
     </div>
