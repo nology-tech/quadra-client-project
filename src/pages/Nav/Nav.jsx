ccc23f7c321336d0f8572e8d4de6842a79a9664a
Chris Wakley <chris.wakley666@gmail.com>
2022-11-03 11:34:17 +0000

    logo component integration edits

--- src/pages/Nav/Nav.jsx
+++ src/pages/Nav/Nav.jsx
@@ -1,37 +1,40 @@
 import "./Nav.scss";
-import Logo from "../../assets/images/logo.png";
+import Logo from "../../components/Logo/Logo";
 import Contacts from "../../assets/images/non-focus-contacts.png";
 import Wallet from "../../assets/images/non-focus-wallet.png";
 import LiveRate from "../../assets/images/non-focus-graph.png";
 import Convert from "../../assets/images/non-focus-convert.png";
-import Transfer from "../../assets/images/transfer-focus.png"
+import Transfer from "../../assets/images/transfer-focus.png";
+
 const Nav = () => {
   return (
     <div className="nav">
-      <Logo />
+      <div className="nav__logo">
+        <Logo isTextDark={true}/>
+      </div>
       <div className="nav__menu">
         <h3>Menu</h3>
-        <div className="menu__button">
+        <div className="nav__button">
           <img src={Contacts} alt="contacts"></img>
           <p>Contacts</p>
         </div>
-        <div className="menu__button">
+        <div className="nav__button">
           <img src={Wallet} alt="wallet"></img>
           <p>Wallet</p>
         </div>
-        <div className="menu__button">
+        <div className="nav__button">
           <img src={LiveRate} alt="graph"></img>
           <p>Live rates</p>
         </div>
-        <div className="menu__button">
+        <div className="nav__button">
           <img src={Convert} alt="convert"></img>
           <p>Convert</p>
         </div>
-        <div className="menu__button">
+        <div className="nav__button">
           <img src={Transfer} alt="transfer"></img>
           <p>Transfer</p>
         </div>
-        <div className="menu__button">
+        <div className="nav__button">
           <img src={Contacts} alt="contacts"></img>
           <p>Contacts</p>
         </div>
