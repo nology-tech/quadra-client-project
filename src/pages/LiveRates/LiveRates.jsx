259a0df2f0bd0891db851be66b25240e1c28bfd2
Zain-Ashfaq <zashfaq00@gmail.com>
2022-11-04 16:57:26 +0000

    styling for currency rate row

--- src/pages/LiveRates/LiveRates.jsx
+++ src/pages/LiveRates/LiveRates.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-
+import LiveRate from "../../components/LiveRate/LiveRate";
 import "./LiveRates.scss";
+import { getCurrencyGBP } from "../../utils/apiUtils";
 
 //Plan
 
@@ -15,24 +16,16 @@ import "./LiveRates.scss";
 // ZAIN COMMENT
 
 const LiveRates = () => {
-  const getCurrency = async () => {
-    // This is default url
-    const url =
-      "https://money-app-api-oi3xuie5la-nw.a.run.app/quadra-money/v1/currencies/gbp";
-
-    const res = await fetch(url);
-    const data = await res.json();
-
-    // console.log(data);
-    console.log("This is USD ", data.rates.USD);
-    console.log("This is USD ", data.rates.USD);
-
-    // console.log("this is data and name", data[0].name);
-  };
-
+  const [currRates, setCurrRates] = useState({});
   useEffect(() => {
-    getCurrency();
+    const getData = async () => {
+      const currencies = await getCurrencyGBP();
+      setCurrRates(currencies);
+    };
+    getData();
   }, []);
+
+  console.log(currRates.AED);
 
   //Button onclick gets current currency abv and adds it do constCurrency
 
@@ -41,15 +34,25 @@ const LiveRates = () => {
   // const addCurrency = () => {};
 
   return (
-    <div>
-      <h1> Transfer </h1>
-      <h1> View Latest Rates </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nam
-        inventore deserunt id? Rem minima dolorem asperiores perferendis sed
-        aspernatur. Dolorem nostrum voluptatibus dolore odit provident minima
-        reiciendis dignissimos iste.
-      </p>
+    <div className="rates">
+      <div className="rates-info">
+        <h1 className="rates-info__transfer"> Transfer </h1>
+        <h1 id="latest-rates"> View Latest Rates </h1>
+        <p className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nam
+          inventore deserunt id? Rem minima dolorem asperiores perferendis sed
+          aspernatur. Dolorem nostrum voluptatibus dolore odit provident minima
+          reiciendis dignissimos iste.
+        </p>
+      </div>
+      <div className="rates-ctner">
+        <div className="rates-ctner__title">
+          <h3>Currency</h3>
+          <h3>Amount</h3>
+          <h3>Rate</h3>
+        </div>
+        <LiveRate name="British Pound" amount="1.00" />
+      </div>
     </div>
   );
 };
