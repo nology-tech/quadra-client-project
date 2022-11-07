4de94ea229c0e15f8274c991e51a303d39b2359e
Zain-Ashfaq <zashfaq00@gmail.com>
2022-11-07 12:30:59 +0000

    Changed App.jsx to default

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-// import { Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import "./styles/main.scss";
-// import Login from "./pages/Login/Login";
-// import SignUp from "./pages/SignUp/SignUp";
-// import Wallet from "./pages/Wallet/Wallet";
-
-import Transfer from "./pages/Transfer/Transfer";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import Wallet from "./pages/Wallet/Wallet";
 
 const App = () => {
   return (
-    // <Routes>
-    //   <Route path="*" element={<Home />} />
-    //   <Route path="/login" element={<Login />} />
-    //   <Route path="/signup" element={<SignUp/>} />
-    //   <Route path="/wallet" element={<Wallet/>} />
-    // </Routes>
-    <Transfer isActive={true} />
+    <Routes>
+      <Route path="*" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/wallet" element={<Wallet />} />
+    </Routes>
   );
 };
 
