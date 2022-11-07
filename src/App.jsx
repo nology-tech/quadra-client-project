0da8e077e366282979d08ad2ac5d69c273043108
haoxle <hao-le@live.co.uk>
2022-11-07 12:33:03 +0000

    removed wallet call

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-// import { Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import "./styles/main.scss";
-// import Login from "./pages/Login/Login";
-// import SignUp from "./pages/SignUp/SignUp"
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 import Wallet from "./pages/Wallet/Wallet";
 
 const App = () => {
   return (
-    // <Routes>
-    //   <Route path="*" element={<Home />} />
-    //   <Route path="/login" element={<Login />} />
-    //   <Route path="/signup" element={<SignUp/>} />
-    // </Routes>
-    <Wallet />
+    <Routes>
+      <Route path="*" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/wallet" element={<Wallet />} />
+    </Routes>
   );
 };
 
