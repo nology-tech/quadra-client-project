fd0a4c3e291b23d95978f172659de0133c9203b0
lanniiee <lav250397@hotmail.com>
2022-11-07 14:16:50 +0000

    modify classname and sizing to rem

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -4,10 +4,10 @@ import cross from "../../assets/images/modal-cross.png";
 const ModalWindow = ({title, toggleModal}) => {
 
     return(
-        <div className="modal-background">
-            <div className="modal">
+        <div className="modal">
+            <div className="modal__content">
                 <h1>{title}</h1>
-                <img className="modal-cross" src={cross} alt="modal cross" onClick={toggleModal} />
+                <img className="modal__image" src={cross} alt="modal cross" onClick={toggleModal} />
                 <hr/>
             </div>
         </div>
