255d80e53f8121ad64ff604dc17d39841443f91d
lanniiee <lav250397@hotmail.com>
2022-11-07 15:03:27 +0000

    added classname for heading & center modal wrapper

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -6,7 +6,7 @@ const ModalWindow = ({title, toggleModal}) => {
     return(
         <div className="modal">
             <div className="modal__content">
-                <h1>{title}</h1>
+                <h1 className="modal__heading">{title}</h1>
                 <img className="modal__image" src={cross} alt="modal cross" onClick={toggleModal} />
                 <hr/>
             </div>
