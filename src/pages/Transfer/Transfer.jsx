e273ab2b92e40e232ba84a818899908a5c668532
Zain-Ashfaq <zashfaq00@gmail.com>
2022-11-07 12:29:47 +0000

    Refactored BEM naming convention

--- src/pages/Transfer/Transfer.jsx
+++ src/pages/Transfer/Transfer.jsx
@@ -1,11 +1,12 @@
 import "./Transfer.scss";
-
+import Button from "../../components/Button/Button";
 const Transfer = ({
-  currencyFlag,
-  currencyName = "GBP",
-  amount = "2.50",
-  rate = "0.00",
-  isActive,
+  sendCurr = "GBP - British Pounds",
+  sendAmount = "£1000.00",
+  recCurr = "USD - US Dollars",
+  recAmount = "$1359.50",
+  rate = 1.3595,
+  fee = "£0.00",
 }) => {
   return (
     <>
@@ -19,18 +20,43 @@ const Transfer = ({
           reiciendis dignissimos iste.
         </p>
       </div>
+      <div className="receipt">
+        <h1 className="receipt__head">You Send</h1>
+        <div className="receipt__box">
+          <h2 className="receipt__currency">{sendCurr}</h2>
+          <h2 className="receipt__currency">{sendAmount}</h2>
+        </div>
+        <h1 className="receipt__head">Recipient gets</h1>
+        <div className="receipt__box">
+          <h2 className="receipt__currency">{recCurr}</h2>
+          <h2 className="receipt__currency">{recAmount}</h2>
+        </div>
 
-      <div
-        className={isActive ? "transfer transfer__active" : "transfer"}
-      ></div>
-      <div className="receipt-container">
-        <img className="transfer__flag" src={currencyFlag} />
-        <p className="transfer__currency">{currencyName}</p>
-        <p className="transfer__amount"> {amount}</p>
-        <p className="transfer__rate">{rate}</p>
-        {/* Put button component here when we have a button element. */}
-        {/* Button text/effect needs to be different depending on isActive */}
-        {/* button class should be "transfer__object__button" for SCSS compatability. */}
+        <div className="receipt__body">
+          <div className="receipt__body__info">
+            <h1 className="receipt__body__info--title">Rate</h1>
+            <h1 className="receipt__body__info--value">{rate}</h1>
+          </div>
+          <div className="receipt__body__info">
+            <h1 className="receipt__body__info--title">Fee</h1>
+            <h1 className="receipt__body__info--value">{fee}</h1>
+          </div>
+          <div className="receipt__body__info">
+            <h1 className="receipt__body__info--title">Delivery</h1>
+            <h1 className="receipt__body__info--value" id="delivery">
+              Typically same day
+            </h1>
+          </div>
+        </div>
+        <div className="receipt__bottom">
+          <div className="receipt__bottom__box">
+            <h1 className="receipt__bottom__box--info">Total</h1>
+            <h1 className="receipt__bottom__box--end">{sendAmount}</h1>
+          </div>
+          <div className="receipt__button">
+            <Button buttonText={"Continue"} />
+          </div>
+        </div>
       </div>
     </>
   );
