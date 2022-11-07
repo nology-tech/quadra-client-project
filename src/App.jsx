3723c4d77bdc2aa85298e91f63ea5659a9bfd54a
haoxle <hao-le@live.co.uk>
2022-11-07 09:26:20 +0000

    removed call on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-// import { Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import "./styles/main.scss";
-// import Login from "./pages/Login/Login";
-// import SignUp from "./pages/SignUp/SignUp"
-import LiveRates from "./pages/LiveRates/LiveRates";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+
 const App = () => {
   return (
-    // <Routes>
-    //   <Route path="*" element={<Home />} />
-    //   <Route path="/login" element={<Login />} />
-    //   <Route path="/signup" element={<SignUp/>} />
-    // </Routes>
-    <LiveRates />
+    <Routes>
+      <Route path="*" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
   );
 };
 
