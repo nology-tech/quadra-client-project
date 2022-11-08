fced1e96304f6be883e4e5b781f7e2e808f0a6f4
haoxle <hao-le@live.co.uk>
2022-11-08 11:19:39 +0000

    reverted app back

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-// import { Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home/Home";
-import LiveRates from "./pages/LiveRates/LiveRates";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import "./styles/main.scss";
-// import Login from "./pages/Login/Login";
-// import SignUp from "./pages/SignUp/SignUp";
-// import Wallet from "./pages/Wallet/Wallet";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import Wallet from "./pages/Wallet/Wallet";
 
 const App = () => {
   return (
-    // <Routes>
-    //   <Route path="*" element={<Home />} />
-    //   <Route path="/login" element={<Login />} />
-    //   <Route path="/signup" element={<SignUp />} />
-    //   <Route path="/wallet" element={<Wallet />} />
-    // </Routes>
-    <LiveRates />
+    <Routes>
+      <Route path="*" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/wallet" element={<Wallet />} />
+    </Routes>
   );
 };
 
