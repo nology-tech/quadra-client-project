dff7ddeb1861e2af3e5ed1c6c89a79e14ced741f
Zain-Ashfaq <zashfaq00@gmail.com>
2022-11-08 11:13:03 +0000

    Reverted button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,12 @@
 import "./Button.scss";
 
-const Button = ({ buttonText, handleClick }) => {
-  return (
-    <>
-      <button className="largeButton" onClick={handleClick}>
-        {buttonText}
-      </button>
-    </>
-  );
-};
+const Button = ({buttonText, handleClick}) =>{
+    return (
+        <>
+            <button className="largeButton" onClick={handleClick}>{buttonText}</button>
+        </>
+    );
+
+}
 
 export default Button;
