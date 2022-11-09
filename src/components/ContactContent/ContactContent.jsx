761184300cdcc435072f23fab95cb09fb3810f34
Nicholas <blueretrograde@gmail.com>
2022-11-09 16:23:53 +0000

    added searchbar & add button

--- src/components/ContactContent/ContactContent.jsx
+++ src/components/ContactContent/ContactContent.jsx
@@ -2,6 +2,8 @@ import "./ContactContent.scss";
 import BankContact from "../BankContact/BankContact";
 import {useEffect, useState} from "react";
 import { getUserContacts } from "../../utils/apiUtils";
+import Searchbar from "../Searchbar/Searchbar";
+import Button from "../../components/Button/Button";
 
 const ContactContent = () => {
     const [allContacts, setAllContacts] = useState([]);
@@ -32,7 +34,11 @@ const ContactContent = () => {
     return (
         <div className="contactList">
             <h3 className="contactList__title">Contact List</h3>
-            <p className="contactList__desc">All your friends and family financial details in one place. Easily transfer currency internationally at the best possible rates.</p>
+            <div>
+                <p className="contactList__desc">All your friends and family financial details in one place. Easily transfer currency internationally at the best possible rates.</p>
+                <Searchbar /> 
+                <Button buttonText={"+ Add"} buttonClass={"addContact"} />
+            </div>
             <div className="contactList__headings">
                 <h5 className="contactList__userName">Name</h5>
                 <h5 className="contactList__sortCode">Sort Code</h5>
