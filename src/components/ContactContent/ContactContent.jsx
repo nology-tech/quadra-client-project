d3fdf9354534e9b2fba69af9d1713eb0875205ad
Lan Anh Vu <110564576+lanniiee@users.noreply.github.com>
2022-11-09 10:02:05 +0000

    Feature/contact page/94 add components styling page (#52)

---
+++ src/components/ContactContent/ContactContent.jsx
@@ -0,0 +1,50 @@
+import "./ContactContent.scss";
+import BankContact from "../BankContact/BankContact";
+import {useEffect, useState} from "react";
+import { getUserContacts } from "../../utils/apiUtils";
+
+const ContactContent = () => {
+    const [allContacts, setAllContacts] = useState([]);
+
+    const getData = async () => {
+        const contacts = await getUserContacts();
+        setAllContacts(contacts)
+    }
+    useEffect(() => {
+        getData();
+    }, [])
+
+    const userContacts = allContacts.map((item) => {
+        return (
+            <BankContact 
+                image_url={""} 
+                userName={item.contactName}
+                sortCode={item.sortCode} 
+                accNum={item.accountNumber} 
+                bank={item.bankName} 
+                iban={""} 
+                isTransfer={false}
+                key={item.id}
+        />
+        )
+    })
+
+    return (
+        <div className="contactList">
+            <h3 className="contactList__title">Contact List</h3>
+            <p className="contactList__desc">All your friends and family financial details in one place. Easily transfer currency internationally at the best possible rates.</p>
+            <div className="contactList__headings">
+                <h5 className="contactList__userName">Name</h5>
+                <h5 className="contactList__sortCode">Sort Code</h5>
+                <h5 className="contactList__accNum">Account No</h5>
+                <h5 className="contactList__bank">Bank</h5>
+                <h5 className="contactList__iban">IBAN</h5>
+            </div>
+            <div className="contactList__list">
+                <div>{userContacts}</div>
+            </div>
+        </div>
+    )
+}
+
+export default ContactContent;
---
+++ src/pages/ContactList/ContactList.jsx
@@ -0,0 +1,13 @@
+import "./ContactList.scss";
+import ContactContent from "../../components/ContactContent/ContactContent";
+
+const ContactList = () => {
+
+    return (
+        <div className="contactList">
+            <ContactContent />
+        </div>
+    )
+}
+
+export default ContactList;
