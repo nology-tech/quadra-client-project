befc30a14df5cf7f3e04bb1818498716fe700591
Lewis Lovett <44164170+LewisLovett@users.noreply.github.com>
2022-11-09 15:15:34 +0000

    Feature/live rates/99 add functionality to liverates page (#55)

--- src/components/LiveRate/LiveRate.jsx
+++ src/components/LiveRate/LiveRate.jsx
@@ -1,27 +1,30 @@
 import "./LiveRate.scss";
 import Button from "../Button/Button";
-import GBP from "../../assets/images/gbp.png";
 const LiveRate = ({
   name,
   amount = "1.00",
   rate,
-  image_url = GBP,
+  flagImage,
   handleClick,
   isCurrentCurrency = true,
 }) => {
   return (
     <div className={isCurrentCurrency ? "live-rate current" : "live-rate"}>
-      <div className="live-rate__currency">
-        <img className="live-rate__icon" src={image_url} />
+      <div className="live-rate__icon">
+        {flagImage}
+      </div>
+      <div className="live-rate__currency">        
         <p className="live-rate__name">{name}</p>
       </div>
       <p className="live-rate__amount">{amount}</p>
       <p className="live-rate__rate">{!isCurrentCurrency && rate}</p>
+      <div className="live-rate__button">
       <Button
         buttonClass={isCurrentCurrency ? "whiteBorderButton" : "corneredButton"}
         buttonText={isCurrentCurrency ? "Edit" : "Send"}
         handleClick={handleClick}
       />
+      </div>
     </div>
   );
 };
