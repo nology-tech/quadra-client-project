e57e087432e7952ec70ca4e2e73e91101d637646
Nicholas <blueretrograde@gmail.com>
2022-11-09 12:58:33 +0000

    added component width to fit the page

--- src/pages/Wallet/Wallet.jsx
+++ src/pages/Wallet/Wallet.jsx
@@ -6,19 +6,17 @@ import LoggedIn from "../../components/LoggedIn/LoggedIn";
 const Wallet = ({ amount = "0", user}) => {
   return (
     <div className="wallet">
-      <div className="wallet-ctner">
         <div className="wallet__user">
           <LoggedIn />
         </div>
-        <div className="wallet-ctner__message">
-          <img src={hand} alt="welcome"></img><p className="welcome__text">Hey {user.email}</p>
+        <div className="wallet__message">
+          <p className="welcome__text">Hey {user.email}</p><img src={hand} alt="welcome"></img>
         </div>
-        <div className="wallet-ctner__funds">
+        <div className="wallet__funds">
           <Funds amount={amount} isManage={true} />
         </div>
-        <div className="wallet-ctner__contacts">
+        <div className="wallet__contacts">
         </div>
-      </div>
     </div>
   );
 };
