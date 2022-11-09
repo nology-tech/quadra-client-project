e18ae6f0a7882693f6f1b82671905b7e33f4f946
Hao <hao-le@live.co.uk>
2022-11-09 09:07:18 +0000

    reverted app back

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-// import { Route, Routes } from "react-router-dom";
-// import { useState } from "react";
-// import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import Home from "./pages/Home/Home";
 import "./styles/main.scss";
-// import Login from "./pages/Login/Login";
-// import SignUp from "./pages/SignUp/SignUp";
-// import Wallet from "./pages/Wallet/Wallet";
-import Transfer from "./pages/Transfer/Transfer";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import Wallet from "./pages/Wallet/Wallet";
 
 const App = () => {
-  // const [user, setUser] = useState();
+  const [user, setUser] = useState();
 
   return (
-    // <Routes>
-    //   <Route path="*" element={<Home />} />
-    //   <Route path="/login" element={<Login saveUser={setUser} />} />
-    //   <Route path="/signup" element={<SignUp />} />
-    //   {user && <Route path="/wallet" element={<Wallet />} />}
-    // </Routes>
-    <Transfer />
+    <Routes>
+      <Route path="*" element={<Home />} />
+      <Route path="/login" element={<Login saveUser={setUser} />} />
+      <Route path="/signup" element={<SignUp />} />
+      {user && <Route path="/wallet" element={<Wallet />} />}
+    </Routes>
   );
 };
 
