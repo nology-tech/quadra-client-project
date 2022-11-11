b716d365c29b248918fa48c743e2f1926bb74bec
Lewis Lovett <44164170+LewisLovett@users.noreply.github.com>
2022-11-11 12:27:23 +0000

    Feature/live rate/86-update-live-rate-and-contact-page-to-match-figma-diagram (#65)

--- src/pages/ContactList/ContactList.jsx
+++ src/pages/ContactList/ContactList.jsx
@@ -1,10 +1,14 @@
 import "./ContactList.scss";
 import ContactContent from "../../components/ContactContent/ContactContent";
+import LoggedIn from "../../components/LoggedIn/LoggedIn";
 
 const ContactList = () => {
 
     return (
         <div className="contactList">
+            <div className="logged-in-container">
+                <LoggedIn/>
+            </div>
             <ContactContent numContacts={10} />
         </div>
     )
