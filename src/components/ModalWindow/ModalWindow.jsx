0a8f7ece2f8727ef8f5bb03e637a7d2dfde75db5
Zain-Ashfaq <zashfaq00@gmail.com>
2022-11-14 09:18:43 +0000

    wip

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -3,25 +3,121 @@ import cross from "../../assets/images/modal-cross.png";
 import magnifyingGlass from "../../assets/images/searchIcon.png";
 import BankContact from "../BankContact/BankContact";
 import { useState } from "react";
+import { useEffect } from "react";
 
-const ModalWindow = ({ title, toggleModal, contacts, pageFrom, onclick }) => {
+const ModalWindow = ({
+  title,
+  toggleModal,
+  contacts,
+  pageFrom,
+  onclick,
+  modalVisible,
+}) => {
+  // console.log("this is contacts in modalWindow ", contacts);
+  // console.log(modalVisible);
   const [inputName, setInputName] = useState("");
-  console.log("value of inputName", inputName);
+  const [filterArray, setFilterArray] = useState([]);
+
+  useEffect(() => {
+    console.log("this is contacts on load", contacts);
+    setFilterArray(...filterArray, contacts);
+    console.log("this is modal visivle on load", modalVisible);
+  }, []);
+
+  // console.log(toggleModal);
+
+  // console.log("value of inputName", inputName);
   const handleInput = (event) => {
     const cleanInput = event.target.value;
-    console.log(cleanInput);
 
-    setInputName(cleanInput.toLowerCase());
+    // space cannot be first character
+
+    if (cleanInput[0] === " ") {
+      return setInputName("");
+    }
+    // cant space back to back
+    if (/^[A-Za-z\s]+$/.test(cleanInput) === true) {
+      return setInputName(cleanInput.toLowerCase().replace(/\s\s+/g, " "));
+    }
+
+    console.log("this is filter array in handleInput ", filterArray);
+
+    // only input a-z
+    return setInputName(cleanInput.toLowerCase());
+
+    // setInputName(cleanInput.toLowerCase());
   };
 
-  let counter = 0;
+  console.log(filterArray);
+
+  // console.log("this is modal visivle", modalVisible);
+  // console.log("this is contacts", contacts);
+  // console.log("this is filter array", filterArray.length);
+
+  const showArray = (arr) => {
+    return arr
+      .sort((a, b) => {
+        if (a.contactName < b.contactName) {
+          return -1;
+        }
+        if (a.contactName > b.contactName) {
+          return 1;
+        }
+        return 0;
+      })
+
+      .map((contact) => {
+        // eslint-disable-next-line no-debugger
+
+        {
+          return (
+            <>
+              <div
+                className="contact-row-wrapper hvr-underline-from-left"
+                onClick={() => {
+                  onclick(contact);
+                  toggleModal();
+                }}
+              >
+                <BankContact
+                  key={contact.id}
+                  userName={contact.contactName}
+                  isTransfer={true}
+                  sortCode={contact.sortCode}
+                  accNum={contact.accountNumber}
+                />
+              </div>
+            </>
+          );
+        }
+      })
+      .splice(0, 3);
+  };
+
+  const filterContacts = () => {
+    setFilterArray(
+      contacts.filter((contact) => {
+        if (contact.contactName.toLowerCase().includes(inputName) === true) {
+          console.log("SIUUUUUU", contact.contactName);
+          return contact.contactName.toLowerCase();
+        }
+        return null;
+      })
+    );
+  };
+  useEffect(() => {
+    filterContacts();
+    // console.log("this is filter array ", filterArray);
+  }, [inputName]);
+
+  // console.log("this is filter array", test);
 
   ////////////////
 
   //   console.log(contacts);
   //   console.log("this is func ", onclick);
   return (
-    <div className="modal">
+    <div className={`modal ${modalVisible ? `__active` : ""}`}>
       <div className="modal__content">
         {pageFrom == "transferSend" ? (
           <>
@@ -49,77 +145,25 @@ const ModalWindow = ({ title, toggleModal, contacts, pageFrom, onclick }) => {
                   ></input>
                 </div>
               </form>
+              <div className="contact-row-wrapper">
+                <div className="bank-contact">
+                  <p className="match-background-color"> BE WHITE </p>
+                  <p className="bank-contact__align">Name</p>
+                  <p className="bank-contact__align" id="align">
+                    Sort Code
+                  </p>
+                  <p className="bank-contact__align">Account Number</p>
+                </div>
+              </div>
 
-              {contacts.map((contact) => {
-                // console.log("this is contacts", contacts);
-                //   console.log("this is inside map", contact.contactName);
-                // return (
-                //   <>
-                //     <div></div>
-                //   </>
-                // );
-                if (counter === 0) {
-                  counter++;
-                  return (
-                    <div key={counter} className="contact-row-wrapper">
-                      <div className="bank-contact">
-                        <p className="match-background-color"> BE WHITE </p>
-                        <p className="bank-contact__align">Name</p>
-                        <p className="bank-contact__align" id="align">
-                          Sort Code
-                        </p>
-                        <p className="bank-contact__align">Account Number</p>
-                      </div>
-                    </div>
-                  );
-                }
+              {
+                filterArray.length === 0 &&
+                /[A-Za-z\s]+$/.test(inputName) == false
+                  ? showArray(contacts)
+                  : showArray(filterArray)
 
-                if (inputName == "") {
-                  return (
-                    <div key={counter}>
-                      <div
-                        className="contact-row-wrapper"
-                        onClick={() => {
-                          onclick(contact);
-                          toggleModal();
-                        }}
-                      >
-                        <BankContact
-                          key={contact.id}
-                          userName={contact.contactName}
-                          isTransfer={true}
-                          sortCode={contact.sortCode}
-                          accNum={contact.accountNumber}
-                        />
-                      </div>
-                    </div>
-                  );
-                } else if (
-                  contact.contactName.toLowerCase().includes(inputName)
-                ) {
-                  return (
-                    <>
-                      <div
-                        className="contact-row-wrapper"
-                        onClick={() => {
-                          onclick(contact);
-                          toggleModal();
-                        }}
-                      >
-                        <BankContact
-                          key={contact.id}
-                          userName={contact.contactName}
-                          isTransfer={true}
-                          sortCode={contact.sortCode}
-                          accNum={contact.accountNumber}
-                        />
-                      </div>
-                    </>
-                  );
-                } else {
-                  return null;
-                }
-              })}
+                // showArray(contacts)
+              }
             </div>
           </>
         ) : (
--- src/pages/TransferSend/TransferSend.jsx
+++ src/pages/TransferSend/TransferSend.jsx
@@ -1,14 +1,14 @@
 import "./TransferSend.scss";
-// import LoggedIn from "../../components/LoggedIn/LoggedIn";
+
 import Button from "../../components/Button/Button";
 import Plus from "../../assets/images/plus.png";
 import ModalWindow from "../../components/ModalWindow/ModalWindow";
-// import BankContact from "../../components/BankContact/BankContact";
+
 import { useState, useEffect } from "react";
 import { getUserContacts } from "../../utils/apiUtils";
 
 const TransferSend = ({
-  user = "Sam",
+  user = "Sam Lastname",
   accNum = "123",
   sortCode = "000",
   total = "£100.00",
@@ -20,14 +20,14 @@ const TransferSend = ({
   const buttonPress = () => {
     alert("button has been pressed");
   };
-  console.log(user);
+  // console.log(user);
 
   const [modalVisible, setModalVisible] = useState(false);
   const [allContacts, setAllContacts] = useState([]);
   const [individualContact, setIndividualContact] = useState([]);
 
   const toggleModal = () => {
-    console.log("modal toggle has been pressed");
+    // console.log("modal toggle has been pressed");
     setModalVisible(!modalVisible);
   };
 
@@ -52,89 +52,100 @@ const TransferSend = ({
     // );
   };
 
-  console.log("this is length ", individualContact.length);
+  // console.log("ignore", getContactData);
 
+  // console.log("this is length ", individualContact.length);
+  console.log("page load all contacts", allContacts);
   return (
     <>
-      {modalVisible ? (
-        <>
-          <ModalWindow
-            toggleModal={toggleModal}
-            title="Choose Recipient"
-            contacts={allContacts}
-            pageFrom="transferSend"
-            onclick={getContactData}
-          ></ModalWindow>
-        </>
-      ) : (
-        <>
-          <div className="info">
-            <h1 className="info__transfer"> Transfer </h1>
-            <h1 className="info__heading"> View Latest Rates </h1>
-            <p className="info__content">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nam
-              inventore deserunt id? Rem minima dolorem asperiores perferendis
-              sed aspernatur. Dolorem nostrum voluptatibus dolore odit provident
-              minima reiciendis dignissimos iste.
-            </p>
-          </div>
-          <div className="transferForm">
-            <h1 className="transferForm__head">Send From</h1>
-            <div className="transferForm__inner">
-              <h1 className="transferForm__user">
+      <ModalWindow
+        key={"modal-window"}
+        toggleModal={toggleModal}
+        title="Choose Recipient"
+        contacts={allContacts}
+        pageFrom="transferSend"
+        onclick={getContactData}
+        modalVisible={modalVisible}
+      ></ModalWindow>
+      <div
+        style={
+          modalVisible
+            ? {
+                opacity: 0.2,
+                backgroundColor: "hsla(0, 0%, 0%, 0.6)",
+                border: "2px solid black",
+                height: "100vh",
+              }
+            : null
+        }
+      >
+        <div className="info">
+          <h1 className="info__transfer"> Transfer </h1>
+          <h1 className="info__heading"> View Latest Rates </h1>
+          <p className="info__content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nam
+            inventore deserunt id? Rem minima dolorem asperiores perferendis sed
+            aspernatur. Dolorem nostrum voluptatibus dolore odit provident
+            minima reiciendis dignissimos iste.
+          </p>
+        </div>
+
+        <div className="transferForm">
+          <h1 className="transferForm__head">Send From</h1>
+          <div className="transferForm__inner">
+            <h1 className="transferForm__user">
+              {Object.keys(individualContact).length > 0
+                ? individualContact.contactName
+                : user}
+            </h1>
+            <div className="transferForm__infoWrapper">
+              <h2 className="transferForm__infoName">Account Number</h2>
+              <p className="transferForm__infoNum">
                 {Object.keys(individualContact).length > 0
-                  ? individualContact.contactName
-                  : user}
-              </h1>
-              <div className="transferForm__infoWrapper">
-                <h2 className="transferForm__infoName">Account Number</h2>
-                <p className="transferForm__infoNum">
-                  {Object.keys(individualContact).length > 0
-                    ? individualContact.accountNumber
-                    : accNum}
-                </p>
-              </div>
-              <div className="transferForm__infoWrapper">
-                <h2 className="transferForm__infoName">Sort Code</h2>
-                <p className="transferForm__infoNum">
-                  {Object.keys(individualContact).length > 0
-                    ? individualContact.sortCode
-                    : sortCode}
-                </p>
-              </div>
-              <div className="transferForm__greyLine"></div>
-              <div className="transferForm__totalWrapper">
-                <h2 className="transferForm__total">Total</h2>
-                <p className="transferForm__total transferForm__total--amount">
-                  {total}
-                </p>
-              </div>
-              <div className="transferForm__remainderWrapper">
-                <h2 className="transferForm__remainder">Funds Remaining</h2>
-                <p className="transferForm__remainder transferForm__remainder--amount">
-                  {fundsRem}
-                </p>
-              </div>
+                  ? individualContact.accountNumber
+                  : accNum}
+              </p>
             </div>
-            <div className="transferForm__sendWrapper">
-              <h1 className="transferForm__head">To</h1>
-              <div className="transferForm__buttons">
-                <div className="transferForm__select" onClick={toggleModal}>
-                  <Button
-                    buttonClass="largeButton"
-                    buttonText="Select Recipient"
-                    // onClick={toggleModal}
-                  />
-                </div>
-                <div className="transferForm__new" onClick={buttonPress}>
-                  <img src={Plus} alt="plus" />
-                  <p>Pay someone new</p>
-                </div>
+            <div className="transferForm__infoWrapper">
+              <h2 className="transferForm__infoName">Sort Code</h2>
+              <p className="transferForm__infoNum">
+                {Object.keys(individualContact).length > 0
+                  ? individualContact.sortCode
+                  : sortCode}
+              </p>
+            </div>
+            <div className="transferForm__greyLine"></div>
+            <div className="transferForm__totalWrapper">
+              <h2 className="transferForm__total">Total</h2>
+              <p className="transferForm__total transferForm__total--amount">
+                {total}
+              </p>
+            </div>
+            <div className="transferForm__remainderWrapper">
+              <h2 className="transferForm__remainder">Funds Remaining</h2>
+              <p className="transferForm__remainder transferForm__remainder--amount">
+                {fundsRem}
+              </p>
+            </div>
+          </div>
+          <div className="transferForm__sendWrapper">
+            <h1 className="transferForm__head">To</h1>
+            <div className="transferForm__buttons">
+              <div className="transferForm__select" onClick={toggleModal}>
+                <Button
+                  buttonText="Select Recipient"
+                  onClick={toggleModal}
+                  buttonClass="largeButton"
+                />
+              </div>
+              <div className="transferForm__new" onClick={buttonPress}>
+                <img src={Plus} alt="plus" />
+                <p>Pay someone new</p>
               </div>
             </div>
           </div>
-        </>
-      )}
+        </div>
+      </div>
     </>
   );
 };
